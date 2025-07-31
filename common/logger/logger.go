96782adcf7d0b94df74ca992e50c87e5445eac51
Maks Zhakun <maks@rainbow.me>
2025-07-31 13:35:26 +0400

    chore: tidy fix

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -154,20 +154,20 @@ func newZapLogger(environment env.Environment) (*Logger, error) {
 	case env.EnvironmentLocal:
 		config = zap.NewDevelopmentConfig()
 		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-		options = append(options, zap.AddStacktrace(zap.PanicLevel))
+		options = append(options, zap.AddStacktrace(zap.ErrorLevel))
 
 	case env.EnvironmentLocalDocker, env.EnvironmentDevelopment, env.EnvironmentStaging:
 		// Development/Staging: JSON logs for Datadog ingestion
 		config = zap.NewProductionConfig()
 		config.EncoderConfig = encoderConfig
-		options = append(options, zap.AddStacktrace(zap.PanicLevel))
+		options = append(options, zap.AddStacktrace(zap.ErrorLevel))
 
 	case env.EnvironmentProduction:
 		// Production: JSON logs with structured metadata
 		config = zap.NewProductionConfig()
 		config.EncoderConfig = encoderConfig
 		config.Level.SetLevel(zap.InfoLevel)
-		options = append(options, zap.AddStacktrace(zap.PanicLevel))
+		options = append(options, zap.AddStacktrace(zap.ErrorLevel))
 	}
 
 	// Build the logger
