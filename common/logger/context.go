876c5323a4dbec5257eebb872b7941d3424edf2e
Martin <MartinBechtle@users.noreply.github.com>
2025-08-14 12:05:10 +0200

    fix stale spans in logger

--- common/logger/context.go
+++ common/logger/context.go
@@ -3,25 +3,37 @@ package logger
 import (
 	"context"
 
+	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
 	"go.uber.org/zap"
 )
 
 type loggerKey struct{}
 
-// FromContext extracts a logger from the context or instantiates a new one if none found
+// FromContext extracts a logger from the context or instantiates a new one if none found, and adds custom fields
+// for tracing.
 func FromContext(ctx context.Context) *Logger {
-	if logger, ok := ctx.Value(loggerKey{}).(*Logger); ok {
-		return logger
+	var fields []zap.Field
+	span, found := tracer.SpanFromContext(ctx)
+	if found {
+		fields = append(fields, WithTrace(span.Context())...)
 	}
+
+	if logger, ok := ctx.Value(loggerKey{}).(*Logger); ok {
+		return logger.With(fields...)
+	}
+
+	// no logger in context, let's grab the global logger
 	log, err := Instance()
 	if err != nil {
+		// in the unlikely event that the global logger failed to start, try creating a production logger
 		z, zErr := zap.NewProduction()
 		if zErr != nil {
+			// if this fails as well, just use a no-op
 			z = zap.NewNop()
 		}
-		return NewLogger(z)
+		log = NewLogger(z)
 	}
-	return log
+	return log.With(fields...)
 }
 
 // ContextWithLogger returns a context with the logger stored for later retrieval via FromContext
